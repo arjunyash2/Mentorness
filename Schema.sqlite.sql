37c2c1d091347c5553c71cf53ce2755169e51647
arjunyash2 <40038029+arjunyash2@users.noreply.github.com>
2024-09-27 19:18:38 +0100

    Add files via upload

---
+++ Schema.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'inventory' ('Store_ID' INTEGER,'Product_ID' INTEGER,'Stock_On_Hand' INTEGER, 
+                          FOREIGN KEY ('Store_ID') REFERENCES stores('store_id'),
+                         FOREIGN KEY ('Product_ID') REFERENCES products(product_id));
+CREATE TABLE 'products' ('Product_ID' INTEGER,'Product_Name' TEXT,'Product_Category' TEXT,'Product_Cost' INTEGER,'Product_Price' INTEGER);
+CREATE TABLE 'sales' ('Sale_ID' INTEGER,
+                      'Date' DATE,
+                      'Store_ID' INTEGER,
+                      'Product_ID' INTEGER,
+                      'Units' INTEGER,
+                     FOREIGN KEY ('Store_ID') REFERENCES stores('store_id'),
+                     FOREIGN KEY ('Product_ID') REFERENCES products('Product_ID'));
+CREATE TABLE 'stores' ('Store_ID' INTEGER,'Store_Name' TEXT,'Store_City' TEXT,'Store_Location' TEXT,'Store_Open_Date' DATE);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
